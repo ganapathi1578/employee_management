48142c8781d8f85966c0f33d4384ad941596ae01
ganapathi1578 <ganapathi1578@gmail.con>
2025-04-16 13:33:07 +0530

    changed the path blank from home to admin

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='core/password_reset.html'), name='password_reset'),
